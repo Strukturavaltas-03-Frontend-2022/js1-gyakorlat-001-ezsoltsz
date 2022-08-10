5359118e768899c353d45c0b183a7324720a51e3
Szabo Zsolt <sz.zsolt97@outlook.hu>
2022-08-10 15:04:12 +0200

    finishit

--- js/sum.js
+++ js/sum.js
@@ -1,2 +1,5 @@
-let summation = (a = 0, b = 0) => a + b
-let subtraction = (a = 0, b = 0) => a - b
+const summation = (a = 0, b = 0) => {
+return a + b }
+
+const subtraction = (a = 0, b = 0) => {
+return a - b }
